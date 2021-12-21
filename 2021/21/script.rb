1522eb3067db1ca1bb5900e4c0bf609e224cfad9
nick <nickagliano@gmail.com>
2021-12-21 13:28:07 -0500

    Fix up

--- 2021/21/script.rb
+++ 2021/21/script.rb
@@ -6,28 +6,30 @@ p2 = 8 - 1 # minus 1 to account for 0 indexed positions
 @universes = {}
 
 def play(p1, p2, s1, s2)
-  return [1, 0] if s1 >= 21
+  return [1, 0] if s1 >= 21 # winner for this universe is player 1
 
-  return [0, 1] if s2 >= 21
+  return [0, 1] if s2 >= 21 # winner for this universe is player 2
 
+  # return the winner of this state--we've seen it in another universe
   return @universes[[p1, p2, s1, s2]] if @universes[[p1, p2, s1, s2]]
 
-  ans = [0, 0]
+  win_count = [0, 0]
 
   [1, 2, 3].each do |x|
     [1, 2, 3].each do |y|
       [1, 2, 3].each do |z|
-        new_position = (p1 + x + y + z) % 10
+        roll_sum = x + y + z
+        new_position = (p1 + roll_sum) % 10
         new_score = s1 + new_position + 1 # plus 1 to account for 0 indexed positions
 
         x1, y1 = play(p2, new_position, s2, new_score)
-        ans = [ans[0] + y1, ans[1] + x1]
-        @universes[[p1, p2, s1, s2]] = ans
+        win_count = [win_count[0] + y1, win_count[1] + x1]
+        @universes[[p1, p2, s1, s2]] = win_count
       end
     end
   end
 
-  ans
+  win_count
 end
 
 puts play(p1, p2, 0, 0).max
